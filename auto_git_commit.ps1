2065e6296dcc619cee0faceae10420f9f51b5fc2
dvvansia1805 <21icdhr025@ldce.ac.in>
2026-07-02 14:26:07 +0200

    Automated commit: 2026-07-02 14:26:07

--- auto_git_commit.ps1
+++ auto_git_commit.ps1
@@ -18,7 +18,7 @@ if ($status) {
     
     $commitMessage = "Automated commit: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
     git commit -m $commitMessage
-    git push origin master
+    git push origin main
     Write-Host "Changes committed and pushed successfully."
 } else {
     Write-Host "No uncommitted changes."
